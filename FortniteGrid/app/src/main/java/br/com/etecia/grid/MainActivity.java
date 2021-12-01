55b849a22891331723caf0337cd1a8a3d956e75f
RyanGaspri <88353449+RyanGaspri@users.noreply.github.com>
2021-12-01 20:18:16 -0300

    Add files via upload

---
+++ FortniteGrid/app/src/main/java/br/com/etecia/grid/MainActivity.java
@@ -0,0 +1,20 @@
+package br.com.etecia.grid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView
+    private ImageView
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
